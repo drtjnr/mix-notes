38fe5e8a442d62629b2a608332a2684387e0b9f7
David Thomas <davethomasjunior@gmail.com>
2025-01-04 12:01:20 -0600

    Initial Commit

---
+++ Mix Notes/Mix_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mix_NotesApp.swift
+//  Mix Notes
+//
+//  Created by David Thomas on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Mix_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mix NotesTests/Mix_NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mix_NotesTests.swift
+//  Mix NotesTests
+//
+//  Created by David Thomas on 1/4/25.
+//
+
+import Testing
+@testable import Mix_Notes
+
+struct Mix_NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
